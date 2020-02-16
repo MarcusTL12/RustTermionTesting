af18768eadf373dffb45b0d726fab8ad38fe8346
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-02-16 15:56:50 +0100

    Board responds to mouse clicks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io::Write;
+use std::{error::Error, io::Write};
 
 use termion::{
     clear, color, cursor,
-    event::{Event, Key, MouseButton},
+    event::{Event, Key, MouseButton, MouseEvent},
     style,
 };
 
@@ -10,8 +10,8 @@ use termion_game_engine::{GameObject, TerminalGameStatic};
 use termion_game_engine_util::{box_mix, Button, TextLabel};
 
 struct Board {
-    _board: Vec<Vec<Option<bool>>>,
-    _turn: bool,
+    board: Vec<Vec<Option<bool>>>,
+    turn: bool,
     pos: (u16, u16),
     n: usize,
 }
@@ -19,72 +19,59 @@ struct Board {
 impl Board {
     fn new(pos: (u16, u16), n: usize) -> Self {
         Board {
-            _board: (0..n).map(|_| (0..n).map(|_| None).collect()).collect(),
-            _turn: false,
+            board: (0..n).map(|_| (0..n).map(|_| None).collect()).collect(),
+            turn: false,
             pos: pos,
             n: n,
+        }
+    }
+    //
+    fn getcell(&self, mpos: (u16, u16)) -> Option<(u16, u16)> {
+        let x = mpos.0 as i16 - self.pos.0 as i16;
+        let y = mpos.1 as i16 - self.pos.1 as i16;
+        if x < 0
+            || x >= (self.n * 4) as i16
+            || y < 0
+            || y >= (self.n * 2) as i16
+            || x % 4 == 0
+            || y % 2 == 0
+        {
+            None
+        } else {
+            Some((x as u16 / 4, y as u16 / 2))
         }
     }
 }
 
 impl GameObject for Board {
-    fn input(&mut self, _e: &Event) {}
+    fn input(&mut self, e: &Event) {
+        match e {
+            Event::Mouse(MouseEvent::Press(MouseButton::Left, x, y)) => {
+                if let Some(cell) = self.getcell((*x, *y)) {
+                    self.board[cell.1 as usize][cell.0 as usize] =
+                        Some(self.turn);
+                    self.turn = !self.turn;
+                }
+            }
+            _ => (),
+        }
+    }
     //
-    // fn render(&mut self, buff: &mut Vec<u8>) {
-    //     write!(
-    //         buff,
-    //         "{}{}",
-    //         cursor::Goto(self.pos.0, self.pos.1),
-    //         color::Fg(color::Reset)
-    //     )
-    //     .unwrap();
-    //     for y in 0..self.n {
-    //         let top = y == 0;
-    //         let upind = !top as usize * 3 * 3 + 3 * 3 * 3;
-    //         for x in 0..self.n {
-    //             let left = x == 0;
-    //             let ind = !left as usize * 1 * 3 + 1 + upind;
-    //             write!(buff, "{}", BOX_MIX[ind]).unwrap();
-    //             for _ in 0..3 {
-    //                 write!(buff, "{}", BOX_MIX[1 + 3]).unwrap();
-    //             }
-    //         }
-    //         write!(buff, "{}", BOX_MIX[3 + upind]).unwrap();
-    //         write!(buff, "\n{}", cursor::Left((self.n * 4 + 1) as u16))
-    //             .unwrap();
-    //         for _ in 0..self.n {
-    //             write!(buff, "{}", BOX_MIX[3 * 3 + 3 * 3 * 3]).unwrap();
-    //             write!(buff, " {} ", ' ').unwrap();
-    //         }
-    //         write!(buff, "{}", BOX_MIX[3 * 3 + 3 * 3 * 3]).unwrap();
-    //         write!(buff, "\n{}", cursor::Left((self.n * 4 + 1) as u16))
-    //             .unwrap();
-    //     }
-    //     for x in 0..self.n {
-    //         let left = x == 0;
-    //         let ind = !left as usize * 1 * 3 + 1 + 3 * 3;
-    //         write!(buff, "{}", BOX_MIX[ind]).unwrap();
-    //         for _ in 0..3 {
-    //             write!(buff, "{}", BOX_MIX[1 + 3]).unwrap();
-    //         }
-    //     }
-    //     write!(buff, "{}", BOX_MIX[3 + 3 * 3]).unwrap();
-    // }
-    fn render(&mut self, buff: &mut Vec<u8>) {
+    fn render(&mut self, buff: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
         write!(
             buff,
-            "{}{}",
-            cursor::Goto(self.pos.0, self.pos.1),
-            color::Fg(color::Reset)
-        )
-        .unwrap();
+            "{}{}Turn: {}{}",
+            color::Fg(color::Reset),
+            cursor::Goto(self.pos.0, self.pos.1 - 1),
+            if self.turn { 'O' } else { 'X' },
+            cursor::Goto(self.pos.0, self.pos.1)
+        )?;
         //
         for y in 0..self.n {
             let top = y == 0;
             let bottom = y == self.n + 1;
             for x in 0..self.n {
                 let left = x == 0;
-                // let right = x == self.n + 1;
                 write!(
                     buff,
                     "{}",
@@ -94,16 +81,12 @@ impl GameObject for Board {
                         if !top { Some(left) } else { None },
                         Some(left)
                     ])
-                )
-                .unwrap();
-                for _ in 0..3 {
-                    write!(
-                        buff,
-                        "{}",
-                        box_mix([Some(top), Some(top), None, None])
-                    )
-                    .unwrap();
-                }
+                )?;
+                write!(
+                    buff,
+                    "{0}{0}{0}",
+                    box_mix([Some(top), Some(top), None, None])
+                )?;
             }
             write!(
                 buff,
@@ -114,19 +97,48 @@ impl GameObject for Board {
                     if !top { Some(true) } else { None },
                     Some(!bottom)
                 ])
-            )
-            .unwrap();
-            write!(buff, "\n{}", cursor::Left((self.n * 4 + 1) as u16))
-                .unwrap();
-            for _ in 0..self.n {
-                write!(buff, "{}", box_mix([None, None, None, None])).unwrap();
-                write!(buff, " {} ", ' ').unwrap();
+            )?;
+            write!(buff, "\n{}", cursor::Left((self.n * 4 + 1) as u16))?;
+            for x in 0..self.n {
+                let left = x == 0;
+                write!(
+                    buff,
+                    "{}",
+                    box_mix([None, None, Some(left), Some(left)])
+                )?;
+                write!(
+                    buff,
+                    " {} ",
+                    if let Some(v) = self.board[y][x] {
+                        ['X', 'O'][v as usize]
+                    } else {
+                        ' '
+                    }
+                )?;
             }
-            write!(buff, "{}", box_mix([None, None, Some(true), Some(true)]))
-                .unwrap();
-            write!(buff, "\n{}", cursor::Left((self.n * 4 + 1) as u16))
-                .unwrap();
+            write!(buff, "{}", box_mix([None, None, Some(true), Some(true)]))?;
+            write!(buff, "\n{}", cursor::Left((self.n * 4 + 1) as u16))?;
         }
+        for x in 0..self.n {
+            let left = x == 0;
+            write!(
+                buff,
+                "{}",
+                box_mix([
+                    Some(true),
+                    if !left { Some(true) } else { None },
+                    Some(left),
+                    None
+                ])
+            )?;
+            write!(
+                buff,
+                "{0}{0}{0}",
+                box_mix([Some(true), Some(true), None, None])
+            )?;
+        }
+        write!(buff, "{}", box_mix([None, Some(true), Some(true), None]))?;
+        Ok(())
     }
 }
 
@@ -140,24 +152,28 @@ struct TicTacToe {
 }
 
 impl TicTacToe {
-    fn new() -> Self {
-        TicTacToe {
+    fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(TicTacToe {
             dbuff: Vec::new(),
             running: true,
-            exitbutton: Button::new((1, 1), (4, 2), color::Red),
+            exitbutton: Button::new((1, 1), (4, 2), color::Red)?,
             exitlabel: TextLabel::new(
                 (5, 2),
                 String::from("<- Exit"),
                 color::Red,
-            ),
-            board: Board::new((20, 10), 3),
+            )?,
+            board: Board::new((1, 5), 3),
             temp: 0,
-        }
+        })
     }
 }
 
 impl TerminalGameStatic for TicTacToe {
-    fn update(&mut self, e: Event, buff: &mut Vec<u8>) {
+    fn update(
+        &mut self,
+        e: Event,
+        buff: &mut Vec<u8>,
+    ) -> Result<(), Box<dyn Error>> {
         self.exitbutton.input(&e);
         self.board.input(&e);
         //
@@ -180,24 +196,15 @@ impl TerminalGameStatic for TicTacToe {
         )
         .unwrap();
         //
-        self.exitbutton.render(buff);
-        self.exitlabel.render(buff);
-        self.board.render(buff);
-        //
-        write!(
-            buff,
-            "{}{}{}",
-            cursor::Goto(1, 20),
-            color::Fg(color::White),
-            // BOX_MIX[self.temp % BOX_MIX.len()]
-            box_mix([Some(true), Some(false), None, Some(true)])
-        )
-        .unwrap();
+        self.exitbutton.render(buff)?;
+        self.exitlabel.render(buff)?;
+        self.board.render(buff)?;
         //
         if let Ok((_, h)) = termion::terminal_size() {
             write!(buff, "{}", cursor::Goto(1, h)).unwrap();
             buff.append(&mut self.dbuff);
         }
+        Ok(())
     }
     //
     fn running(&self) -> bool {
@@ -206,6 +213,6 @@ impl TerminalGameStatic for TicTacToe {
 }
 
 fn main() {
-    let mut game = TicTacToe::new();
-    game.start();
+    let mut game = TicTacToe::new().expect("Game to load");
+    game.start().expect("Game to start");
 }
